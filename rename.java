26ba0b5dd43809d9c116e3a0b5283803ab8b6829
werkstatt <werkstatt@debian>
2017-09-11 16:31:10 +0200

    rename function in NIO.2

---
+++ rename.java
@@ -0,0 +1,23 @@
+import java.nio.file.*;
+import java.io.IOException;
+
+public class rename {
+
+    public static void rename (Path infile, Path outfile) {
+        try {
+            Files.copy(infile, outfile);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+
+    public static void main (String[] args) {
+        //Path infile = Paths.get( "/home/werkstatt/IdeaProjects/rename/src/neuerOrdner/test.txt" );
+        //Path outfile = Paths.get( "/home/werkstatt/IdeaProjects/rename/src/neuerOrdner/testKopie.txt" );
+        Path infile = Paths.get( "src/neuerOrdner/test.txt" );
+        Path outfile = Paths.get( "src/neuerOrdner/testKopie.txt" );
+
+        rename(infile, outfile);
+    }
+}
