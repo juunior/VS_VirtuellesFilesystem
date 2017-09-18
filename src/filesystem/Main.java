b70108e6be29421b7a8278631af09846f40272e9
THOMAS\Thomas <thomas.winkler121@gmail.com>
2017-09-18 14:02:02 +0200

    GUI is working now, when you start the Main

--- src/filesystem/Main.java
+++ src/filesystem/Main.java
@@ -1,6 +1,5 @@
 package filesystem;
 
-import filesystem.GUI.help.HelpFileThread;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -13,10 +12,10 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        (new HelpFileThread()).start(); // load to help file from the resource folder to the local temp folder.
+        //(new HelpFileThread()).start(); // load to help file from the resource folder to the local temp folder.
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Virtual Filesystem");
+        Parent root = FXMLLoader.load(getClass().getResource("/filesystem/GUI/sample.fxml"));
+        primaryStage.setTitle("Virtuelles Filesystem");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -25,5 +24,38 @@ public class Main extends Application{
                 FileSystemManger.getInstance().close();
             }
         });
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 2) {
+            //vs1.usage();
+        }
+        String mode = "client";
+        if (args.length > 1) {
+            // second parameter equals "s"
+            if (args[1].equals("s")) {
+                mode = "server";
+            }
+        }
+        else if (args.length == 1) {
+            // only one parameter -> default
+            mode = "default";
+        }
+
+        /*switch (mode) {
+            case "client":
+                FileSystemManger.getInstance().initClientOnlyMode();
+                break;
+
+            case "server":
+                FileSystemManger.getInstance().initServerOnlyMode(args[0], TCPParallelServer.DEFAULT_PORT);
+                break;
+
+            default:
+                FileSystemManger.getInstance().init(args[0], TCPParallelServer.DEFAULT_PORT);
+        }*/
+
+
+        launch(args);
     }
 }
