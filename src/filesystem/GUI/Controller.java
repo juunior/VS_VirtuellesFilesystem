6093f0f40d7dbe057b78dd50db8aafb92daf7ec0
THOMAS\Thomas <thomas.winkler121@gmail.com>
2017-09-18 15:14:05 +0200

    added methods from fxml

--- src/filesystem/GUI/Controller.java
+++ src/filesystem/GUI/Controller.java
@@ -2,38 +2,25 @@ package filesystem.GUI;
 
 import filesystem.FileSystemManger;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 
+import java.awt.*;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable{
+
+    public void home(ActionEvent actionEvent) {
+    }
+
+    public void refresh(ActionEvent actionEvent) {
+    }
 
     public void close(ActionEvent actionEvent) {
         System.exit(0);
     }
-
-    /*public void search() {
-        String searchStr = textFieldSearch.getText();
-        if (searchStr == null || searchStr.isEmpty()) return;
-
-        searchResults.clear();
-        List<FSObject> list;
-        try {
-            list = fileSystem.search(searchStr);
-            for (FSObject fsObject : list) {
-
-                searchResults.add(
-                        new SearchItem(fsObject.getName(), (fsObject instanceof Folder), fsObject.getPermissions(), fsObject.getParentFolder().getAbsolutePath()));
-            }
-        } catch (Throwable e) {
-            if (FileSystemManger.DEBUG) {
-                e.printStackTrace();
-            }
-            showErrorMessage(e);
-        }
-        tableViewSearch.setItems(searchResults);
-        tabPane.getSelectionModel().select(tabSearch);
-    }*/
 
     public void showErrorMessage(Throwable e) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -41,4 +28,54 @@ public class Controller {
         alert.showAndWait();
     }
 
+    public void mount(ActionEvent actionEvent) {
+    }
+
+    public void toggleLock(ActionEvent actionEvent) {
+    }
+
+    public void createDir(ActionEvent actionEvent) {
+    }
+
+    public void createFile(ActionEvent actionEvent) {
+    }
+
+    public void rename(ActionEvent actionEvent) {
+    }
+
+    public void delete(ActionEvent actionEvent) {
+    }
+
+    public void openHelp(ActionEvent actionEvent) {
+        try {
+            Desktop.getDesktop().open(FileSystemManger.getInstance().getHelpFile());
+        } catch (Throwable e) {
+            Alert io = new Alert(Alert.AlertType.ERROR);
+            io.setTitle("Error");
+            io.setContentText("Could not open Help PDF!");
+            io.showAndWait();
+        }
+    }
+
+    public void showAbout(ActionEvent actionEvent) {
+        Alert about = new Alert(Alert.AlertType.INFORMATION);
+        about.setTitle("About");
+        about.setHeaderText("Virtuelles Filesystem v9000");
+        about.setContentText("\u00A9 2017 by richtig kuhle bois ey");
+        about.showAndWait();
+    }
+
+    public void changeDirectoryManually(ActionEvent actionEvent) {
+    }
+
+    public void search(ActionEvent actionEvent) {
+    }
+
+    public void clearSelectedTab(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
