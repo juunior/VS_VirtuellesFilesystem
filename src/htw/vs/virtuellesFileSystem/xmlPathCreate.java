44f935ac9163ff0858dead2cd992e7516c352ae5
kaizoor <kai.schultz9612@gmail.com>
2017-09-18 16:44:53 +0200

    print one dir and first subdir is working

--- src/htw/vs/virtuellesFileSystem/xmlPathCreate.java
+++ src/htw/vs/virtuellesFileSystem/xmlPathCreate.java
@@ -1,19 +1,14 @@
 package htw.vs.virtuellesFileSystem;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.print.Doc;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 
 public class xmlPathCreate {
@@ -31,21 +26,17 @@ public class xmlPathCreate {
         return doc;
     }
 
-
-    private int readDir(Document doc, String[] files, String dirName) {
+    private Element buildElement(String[] files, String dirName, String eDir) {
         Element p;
-        String eDir = dirName; //eDir ist XML conform
-        if (dirName.contains(" ")) {
-            eDir = dirName.replace(" ", "-_-");
-        }
         if (dirName.startsWith(".")) {
             p = new Element(eDir.substring(1));
-            p.setAttribute("name", dirName);
             p.setAttribute("dotdir", "yes");
+        } else if (Character.isDigit(dirName.charAt(0))) {
+            p = new Element("_" + eDir);
         } else {
             p = new Element(eDir);
-            p.setAttribute("name", dirName);
         }
+        p.setAttribute("name", dirName);
 
         if (files != null) {
             for (String file : files) {
@@ -56,123 +47,82 @@ public class xmlPathCreate {
                 }
                 if (file.startsWith(".")) {
                     e1 = new Element(eDir.substring(1));
-                    e1.setAttribute("name", file);
                     e1.setAttribute("dotfile", "yes");
+                } else if (Character.isDigit(file.charAt(0))) {
+                    e1 = new Element("_" + eDir);
                 } else {
                     e1 = new Element(eDir);
-                    e1.setAttribute("name", file);
                 }
+                e1.setAttribute("name", file);
                 e1.setAttribute("file", "true");
                 p.addContent(e1);
             }
 
         }
+        return p;
+    }
+
+    private String readDir(Document doc, String[] files, String dirName) {
+        Arrays.sort(files);
+        String eDir = dirName; //eDir ist XML conform
+        if (dirName.contains(" ")) {
+            eDir = dirName.replace(" ", "-_-");
+        }
+        Element p = buildElement(files, dirName, eDir);
         doc.getRootElement().addContent(p);
 
-        return doc.getRootElement().indexOf(p);
+        return p.getName();
     }
-    private void readDir(Document doc, String[] files, String dirName, int parent) {
-        Element p;
+
+    private void readParentDir(Document doc, String[] files, String dirName, String child) {
         String eDir = dirName; //eDir ist XML conform
         if (dirName.contains(" ")) {
             eDir = dirName.replace(" ", "-_-");
         }
-        if (dirName.startsWith(".")) {
-            p = new Element(eDir.substring(1));
-            p.setAttribute("name", dirName);
-            p.setAttribute("dotdir", "yes");
-        } else {
-            p = new Element(eDir);
-            p.setAttribute("name", dirName);
-        }
+        Element p = buildElement(files, dirName, eDir);
 
-        if (files != null) {
-            for (String file : files) {
-                eDir = file;
-                Element e1;
-                if (file.contains(" ")) {
-                    eDir = file.replace(" ", "-_-");
-                }
-                if (file.startsWith(".")) {
-                    e1 = new Element(eDir.substring(1));
-                    e1.setAttribute("name", file);
-                    e1.setAttribute("dotfile", "yes");
-                } else {
-                    e1 = new Element(eDir);
-                    e1.setAttribute("name", file);
-                }
-                e1.setAttribute("file", "true");
-                p.addContent(e1);
-            }
-
-        }
-        doc.getRootElement().getChild("").addContent(p);
-
+        doc.getRootElement().getChild(child).addContent(p);
 
 
     }
-
 
 
     private void writeDoc(Document doc) {
 
         File rootDir = new File("/home/kai/studium/");
 
-        System.out.println(getLastSubDir(rootDir, doc));
+        getDir(rootDir, doc);
     }
 
-    private String getLastSubDir(File file, Document doc) {
+    private void buildRec(String[] directories, Document doc, File file, String child) {
+        File file_tmp = null;
+        for (String dir : directories) {
+            file_tmp = new File(file.getAbsolutePath() + "/" + dir);
+            String[] files = file_tmp.list((current, name) -> new File(current, name).isFile());
+            readParentDir(doc, files, dir, child);
+        }
+        if (directories.length != 0) {
+            for (String dir : directories) {
+                file = new File(file.getAbsolutePath() + "/" + dir);
+                buildRec(directories, doc, file, child);
+            }
+        }
+    }
+
+
+    private void getDir(File file, Document doc) {
         String[] directories = file.list((current, name) -> new File(current, name).isDirectory());
-        File subroot = null;
-        boolean goUp = false;
-        boolean depthGot = false;
-        int i = 0;
-        int subDepth = 0;
-        String child = "";
 
-//        while ((directories != null) && (directories.length != 0)) {
-        do{
-            try {
-                if (subroot != null) {
-                    subroot = new File(subroot.getAbsoluteFile() + "/" + directories[i]);
-                    if(!depthGot){subDepth++;}
-                } else {
-                    subroot = new File(file.getAbsoluteFile() + "/" + directories[i]);
-                    if(!depthGot){subDepth++;}
-                }
-            }catch (ArrayIndexOutOfBoundsException e){
-                subDepth = 0;
-            }
-            directories = subroot.list((current, name) -> new File(current, name).isDirectory());
-            if (directories.length == 0){
-                depthGot = true;
-                goUp = true;
-                i++;
-            }
-            if(goUp) {
-                String[] files = subroot.list((current, name) -> new File(current, name).isFile());
-                int parent = readDir(doc, files, subroot.getName());
-                child = subroot.getName();
-                goUp = false;
-                files = new File(subroot.getParent()).list((current, name) -> new File(current, name).isFile());
-                readDir(doc, files, new File(subroot.getParent()).getName(), parent);
-                directories = new File(subroot.getParent()).list((current, name) -> new File(current, name).isDirectory());
-                subDepth= 0;
-            }
-        }while (subDepth != 0);
+        if (directories != null) {
+            Arrays.sort(directories);
+        }
 
+        String[] files = file.list((current, name) -> new File(current, name).isFile());
+        String child = readDir(doc, files, file.getName());
 
+        buildRec(directories, doc, file, child);
 
-
-        System.out.println(subroot.getParent());
-
-
-        return subroot.getName();
     }
-
-//    Element buildParent(Document doc,String[] directories, Element parent){
-//
-//    }
 
     private void writeXML(Document doc) {
         Format format = Format.getPrettyFormat();
