dd1b034ba972c9da9530f40e87206c10afad612f
wolf <cwolf6@gmx.com>
2017-09-19 22:41:50 +0200

    searchXML.java added

---
+++ src/htw/vs/virtuellesFileSystem/searchXML.java
@@ -0,0 +1,45 @@
+package htw.vs.virtuellesFileSystem;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.File;
+import java.io.IOException;
+
+public class searchXML {
+
+    public static void  main(String[] args) throws org.xml.sax.SAXException, IOException,
+            ParserConfigurationException, TransformerException {
+
+        // File Name aendern
+        String str = "test";
+        System.out.println(suche(str));
+    }
+
+    public static String suche(String str) throws ParserConfigurationException, IOException, SAXException {
+
+        String inv = "nicht gefunden.";
+
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document document = docBuilder.parse(new File("xmlTest.xml"));
+
+        NodeList nodeList = document.getElementsByTagName(str);
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+//                System.out.println(node.getNodeName());
+                return node.getNodeName();
+            } else return (inv);
+        }
+        return String.valueOf(inv);
+    }
+}
