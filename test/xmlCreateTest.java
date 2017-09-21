491cff1606716c442b69ee0c3401dcf939f270a4
kaizoor <kai.schultz9612@gmail.com>
2017-09-21 23:16:16 +0200

    some new illegal character

--- test/xmlCreateTest.java
+++ test/xmlCreateTest.java
@@ -9,7 +9,7 @@ public class xmlCreateTest {
         xmlPathCreate jds = new xmlPathCreate();
         Document doc = jds.createDoc("VSFS");
         try {
-            jds.writeDoc(doc, "C:\\Users\\kai\\Desktop\\netio132");
+            jds.writeDoc(doc, "/home/kai/studium/");
         } catch (FileNotFoundException e) {
             System.out.println("File nicht gefunden");
         } catch (NotDirectoryException e) {
