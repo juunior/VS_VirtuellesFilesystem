97cb86fec7d01c6b8ea8c4da6555643b98aadb8e
kaizoor <kai.schultz9612@gmail.com>
2017-09-22 19:00:28 +0200

    typo changed

---
+++ src/htw/vs/virtuellesFileSystem/searchXML.java
@@ -0,0 +1,44 @@
+package htw.vs.virtuellesFileSystem;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.File;
+import java.io.IOException;
+
+public class searchXML {
+
+
+
+
+    public static void suche(String str, Document document) throws ParserConfigurationException, IOException, SAXException {
+
+        NodeList nodeList = document.getElementsByTagName(str);
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(nodeToString(node));
+            }
+        }
+    }
+
+    public static String nodeToString(Node node) {
+        Node tmp = node;
+        String str = "/" + node.getNodeName();
+
+        do {
+            tmp = tmp.getParentNode();
+            if (tmp != null) {
+                str = "/" + tmp.getNodeName() + str;
+            }
+        } while (tmp.getParentNode() != null);
+        str = str.substring(10);// cut first #document
+        return(str);
+    }
+}
