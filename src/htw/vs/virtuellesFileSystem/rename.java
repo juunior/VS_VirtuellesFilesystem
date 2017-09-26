04d1b6219dd6fe07bbd843da2abbc85252088713
wolf <cwolf6@gmx.com>
2017-09-26 20:14:21 +0200

    neue rename + move

--- src/htw/vs/virtuellesFileSystem/rename.java
+++ src/htw/vs/virtuellesFileSystem/rename.java
@@ -5,12 +5,23 @@ import java.io.IOException;
 
 public class rename {
 
-    public static void rename (Path infile, Path outfile) {
-        try {
-            Files.copy(infile, outfile);
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+    /**
+     * benennt datei um
+     * @param source
+     * @param newName
+     * @throws IOException
+     */
+    public static void rename (Path source, String newName) throws IOException {
+        Files.move(source, source.resolve(newName));
     }
 
+    /**
+     * verschiebt Datei in anderen Ordner
+     * @param source
+     * @param dest
+     * @throws IOException
+     */
+    public static void moveTo (Path source, Path dest) throws IOException {
+        Files.move(source, dest.resolve(source.getFileName()));
+    }
 }
