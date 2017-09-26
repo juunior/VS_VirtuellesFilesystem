ef65b8d87f53ca74265adcb109eaf705e3ee873c
kaizoor <kai.schultz9612@gmail.com>
2017-09-26 19:57:41 +0200

    umgebaut und kommentiert und JavaDoc generiert

--- test/xmlCreateTest.java
+++ test/xmlCreateTest.java
@@ -6,15 +6,12 @@ import java.nio.file.NotDirectoryException;
 
 public class xmlCreateTest {
     public static void main(String[] args) {
-        xmlPathCreate jds = new xmlPathCreate();
-        Document doc = jds.createDoc("VSFS");
         try {
-            jds.writeDoc(doc, "/home/kai/studium/");
+            xmlPathCreate.createXML("/home/kai/studium/");
         } catch (FileNotFoundException e) {
             System.out.println("File nicht gefunden");
         } catch (NotDirectoryException e) {
             System.out.println("Pfad ist kein Ordner");
         }
-        jds.writeXML(doc);
     }
 }
