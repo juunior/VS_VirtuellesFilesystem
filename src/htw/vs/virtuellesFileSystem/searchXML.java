39f208904ef60d49e78f3c8cc83082f7551a2c24
juunior <4711Ch88>
2017-09-26 16:33:38 +0200

    getAttributeByString added

--- src/htw/vs/virtuellesFileSystem/searchXML.java
+++ src/htw/vs/virtuellesFileSystem/searchXML.java
@@ -1,6 +1,7 @@
 package htw.vs.virtuellesFileSystem;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -14,18 +15,24 @@ public class searchXML {
     public static void suche(String str, Document document) throws ParserConfigurationException, IOException, SAXException {
 
         str = xmlPathCreate.removeIllegalCharacter(str);
-
         NodeList nodeList = document.getElementsByTagName(str);
 
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(getAttributeByString(node, "name"));
+                System.out.println("file: " + getAttributeByString(node, "file"));
+                System.out.println("directory: " + getAttributeByString(node, "directory"));
+                System.out.print(getAttributeByString(node, "Host") + ":");
                 System.out.println(xmlPathCreate.revertIllegalCharacter(nodeToString(node)));
             }
         }
     }
 
-
+    private static String getAttributeByString(Node node, String str) {
+        Element e = (Element) node;
+        return e.getAttribute(str);
+    }
 
     private static String nodeToString(Node node) {
         Node tmp = node;
